33531751747af1d45e2d691ce4a6ba21e25aa9a5
Joshua Miller <joshdan65@gmail.com>
2020-12-17 09:57:06 -0600

    :mag: add acceptance tests for CLI
    * Even though this does not add to the code coverage metric, it does
      allow us to gain some certainty about the functionality of our
      application, thus allowing for future refactoring efforts

---
+++ git_grappler_cli_smoke_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"testing"
+)
+
+var testFolderName = ".test_bin"
+var binaryName = "Grapple"
+
+func getOsDependentCommandPath() string {
+	var os = runtime.GOOS
+
+	switch os {
+	case "windows":
+		return fmt.Sprintf("%s/%s.exe", testFolderName, binaryName)
+	case "linux":
+		return fmt.Sprintf("%s/%s", testFolderName, binaryName)
+	default:
+		var panicMessage = fmt.Sprintf("Operating system of '%s' not supported for acceptance tests!", os)
+		panic(panicMessage)
+	}
+}
+
+func TestMain(m *testing.M) {
+	os.Mkdir(testFolderName, 0755)
+
+	var commandToRun = exec.Command("go", "build", "-o", testFolderName)
+	var runError = commandToRun.Run()
+
+	if runError != nil {
+		var panicMessage = fmt.Sprintf("Failed to prepare for tests!")
+		panic(panicMessage)
+	}
+
+	var exitCode = m.Run()
+
+	var removeError = os.RemoveAll(testFolderName)
+
+	if removeError != nil {
+		var panicMessage = fmt.Sprintf("Failed to remove test folder: '%s'", testFolderName)
+		panic(panicMessage)
+	}
+
+	os.Exit(exitCode)
+}
+
+func TestCliSaysHello(t *testing.T) {
+	var flagToTest = "hello"
+	var output, err = exec.Command(getOsDependentCommandPath(), flagToTest).Output()
+
+	if err != nil {
+		t.Errorf("Command failed to run: '%s'", err)
+		return
+	}
+
+	var expectedOutput = `Hello! We hope you are enjoying Grapple!
+`
+	var actualOutput = string(output)
+	if actualOutput != expectedOutput {
+		t.Errorf("Output incorrect! Expected '%s', received '%s'", expectedOutput, actualOutput)
+	}
+}
