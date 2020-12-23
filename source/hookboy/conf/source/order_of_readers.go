96839b410cac945e92c96d9007445a498096876c
Joshua Miller <joshdan65@gmail.com>
2020-12-23 15:33:35 -0600

    Reordered how configuration gets read
    * Wanted to allow for explicit overriding by using the most specific configuration files first

--- source/hookboy/conf/source/order_of_readers.go
+++ source/hookboy/conf/source/order_of_readers.go
@@ -5,21 +5,21 @@ import (
 )
 
 var configurationReaders = []configurationReader{
-	packageJSONFileReader{
-		Desc: "Hookboy will use the package.json file for its configuration source if a hookboy section is found within it.",
-	},
-	localFolderReader{
-		Path: ".hookboy",
-		Desc: "Hookboy will look first look for raw hooks in a .hookboy folder, then will look for configuration in a yaml file named .hookboy",
+	localFileReader{
+		Path:         ".hookboy.yml",
+		Desc:         "Hookboy will look for configuration in a yaml file named .hookboy",
+		Deserializer: deserialization.YamlDeserializer{},
 	},
 	localFileReader{
 		Path:         ".hookboy",
 		Desc:         "Hookboy will look first look for raw hooks in a .hookboy folder, then will look for configuration in a yaml file named .hookboy",
 		Deserializer: deserialization.YamlDeserializer{},
 	},
-	localFileReader{
-		Path:         ".hookboy.yml",
-		Desc:         "Hookboy will look for configuration in a yaml file named .hookboy",
-		Deserializer: deserialization.YamlDeserializer{},
+	packageJSONFileReader{
+		Desc: "Hookboy will use the package.json file for its configuration source if a hookboy section is found within it.",
+	},
+	localFolderReader{
+		Path: ".hookboy",
+		Desc: "Hookboy will look first look for raw hooks in a .hookboy folder, then will look for configuration in a yaml file named .hookboy",
 	},
 }
