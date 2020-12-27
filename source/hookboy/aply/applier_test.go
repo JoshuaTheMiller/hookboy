c6fcffd1c58f38519f1d4c373e3c6c919a3ff44d
Joshua Miller <joshdan65@gmail.com>
2020-12-27 11:41:15 -0600

    :zzz: remove test

--- source/hookboy/aply/applier_test.go
+++ source/hookboy/aply/applier_test.go
@@ -33,43 +33,6 @@ func TestGenerateExpectedLineFromFile(t *testing.T) {
 	}
 }
 
-// // More of an Acceptance Test, as it is fairly high level and depends on the file system
-// func TestGeneratedFileIsAsExpected(t *testing.T) {
-// 	var fileName = "samplefortest"
-// 	var linesToAdd = []string{"line1", "line2"}
-// 	createActualGitHookFile(fileName, linesToAdd)
-
-// 	var filePath = fmt.Sprintf(".git/hooks/%s", fileName)
-// 	var contentBytes, error = ioutil.ReadFile(filePath)
-
-// 	if error != nil {
-// 		t.Errorf("File generation appears broken: %s", error.Error())
-// 	}
-
-// 	var actualContents = string(contentBytes)
-// 	var expectedContents = `#!/bin/sh
-// retVal0=line1
-// retVal0=$?
-// retVal1=line2
-// retVal1=$?
-
-// if [ $retVal0 -ne 0 ] || [ $retVal1 -ne 0 ];
-// then
-// exit 1
-// fi
-// exit 0`
-
-// 	if expectedContents != actualContents {
-// 		t.Errorf("Generated file incorrect. Expected '%s', found '%s'", expectedContents, actualContents)
-// 	}
-
-// 	var fileRemoveError = os.Remove(filePath)
-
-// 	if fileRemoveError != nil {
-// 		t.Errorf("Test cleanup failed! Unable to remove file at '%s': '%s'", filePath, fileRemoveError.Error())
-// 	}
-// }
-
 func TestGenerateStatementFileIsAsExpected(t *testing.T) {
 	var statementName = "SomeStatement"
 	var someStatement = "SomeStatement"
