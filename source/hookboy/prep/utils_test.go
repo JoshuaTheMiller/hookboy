61be5177c113cbd2a4aa73fb4469d5aa202d580c
Joshua Miller <joshdan65@gmail.com>
2020-12-28 13:25:15 -0600

    :mag: add test for panic case of itemExists

--- source/hookboy/prep/utils_test.go
+++ source/hookboy/prep/utils_test.go
@@ -1,8 +1,10 @@
 package prep
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestItemExists(t *testing.T) {
+func Test_itemExists_ReturnsTrueWhenItemDoesIndeedExist(t *testing.T) {
 	var someItem = "what"
 	var someArray = [...]string{someItem, "what2"}
 
@@ -13,4 +15,23 @@ func TestItemExists(t *testing.T) {
 	if !itemExists {
 		t.Error("Item existed, but not found")
 	}
+}
+
+func Test_itemExists_PanicsWhenNotArray(t *testing.T) {
+	var someItem = "what"
+	var someArray = "thisisastring"
+
+	defer func() {
+		err := recover()
+		if err == nil {
+			t.Error("Expected panic, did not appear to panic")
+		}
+
+		var exectedMessage = "Invalid data-type"
+		if err != exectedMessage {
+			t.Errorf("e: '%s', a: '%s'", exectedMessage, err)
+		}
+	}()
+
+	itemExists(someArray, someItem)
 }
