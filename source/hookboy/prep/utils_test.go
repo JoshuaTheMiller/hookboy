f0f03f774080f8e33d7aab0b2c41c7731f1c54ce
Joshua Miller <joshdan65@gmail.com>
2020-12-28 12:30:19 -0600

    Remove files from experiment

--- source/hookboy/prep/utils_test.go
+++ source/hookboy/prep/utils_test.go
@@ -2,122 +2,15 @@ package prep
 
 import "testing"
 
-type testObject struct {
-	Type string
-	Name string
-}
+func TestItemExists(t *testing.T) {
+	var someItem = "what"
+	var someArray = [...]string{someItem, "what2"}
 
-func (t testObject) ToMapItem() mapItem {
-	return mapItem{
-		Key:   t.Type,
-		Value: t,
-	}
-}
+	// itemExists is not very clear as far as what param goes where
+	// #CopyPasteFails
+	var itemExists = itemExists(someArray, someItem)
 
-func TestToMapOfGroups(t *testing.T) {
-	var someObjects = []mappable{
-		testObject{
-			Type: "A",
-			Name: "1",
-		},
-		testObject{
-			Type: "A",
-			Name: "2",
-		},
-		testObject{
-			Type: "B",
-			Name: "1",
-		},
-	}
-
-	var mapOfSomeObjects = toMapOfGroups(someObjects...)
-
-	if len(mapOfSomeObjects) != 2 {
-		t.Errorf("Expected map to contain 2 groups")
-	}
-
-	var aLength = len(mapOfSomeObjects["A"])
-	if aLength != 2 {
-		t.Errorf("Expected group of 'A' to contain 2 items, contains %d", aLength)
-	}
-
-	if len(mapOfSomeObjects["B"]) != 1 {
-		t.Errorf("Expected group of 'B' to contain 1 item")
-	}
-}
-
-func (t testObject) SelectKey() interface{} {
-	return t.Type
-}
-
-func (t testObject) SelectValue() interface{} {
-	return t
-}
-
-func TestGroupBy(t *testing.T) {
-	var someObjects = []groupable{
-		testObject{
-			Type: "A",
-			Name: "1",
-		},
-		testObject{
-			Type: "A",
-			Name: "2",
-		},
-		testObject{
-			Type: "B",
-			Name: "1",
-		},
-	}
-
-	var group = groupBy(someObjects)
-
-	if len(group) != 2 {
-		t.Errorf("Expected map to contain 2 groups")
-	}
-
-	var aLength = len(group["A"])
-	if aLength != 2 {
-		t.Errorf("Expected group of 'A' to contain 2 items, contains %d", aLength)
-	}
-
-	if len(group["B"]) != 1 {
-		t.Errorf("Expected group of 'B' to contain 1 item")
-	}
-}
-
-// This would be really awesome with generics
-func TestGroupByKey(t *testing.T) {
-	var someObjects = []interface{}{
-		testObject{
-			Type: "A",
-			Name: "1",
-		},
-		testObject{
-			Type: "A",
-			Name: "2",
-		},
-		testObject{
-			Type: "B",
-			Name: "1",
-		},
-	}
-
-	var keySelect = func(o interface{}) interface{} { return o.(testObject).Type }
-	var valueSelect = func(o interface{}) interface{} { return o }
-
-	var grouping = groupByKey(someObjects, keySelect, valueSelect)
-
-	if grouping.Count() != 2 {
-		t.Errorf("Expected map to contain 2 groups")
-	}
-
-	var aLength = len(grouping.Get("A"))
-	if aLength != 2 {
-		t.Errorf("Expected group of 'A' to contain 2 items, contains %d", aLength)
-	}
-
-	if len(grouping.Get("B")) != 1 {
-		t.Errorf("Expected group of 'B' to contain 1 item")
+	if !itemExists {
+		t.Error("Item existed, but not found")
 	}
 }
