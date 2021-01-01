aa024a3f3cc11a903d550d266badc121e1271473
Joshua Miller <joshdan65@gmail.com>
2021-01-01 16:20:14 -0600

    :soon: abstract os dependencies for easier testing
    * Continue to have integration test for sanity

--- source/hookboy/aply/applier.go
+++ source/hookboy/aply/applier.go
@@ -9,48 +9,53 @@ import (
 	"github.com/hookboy/source/hookboy/internal"
 )
 
+type fileWriter func(filename string, data []byte, perm os.FileMode) error
+type folderCreator func(path string, perm os.FileMode) error
+
 type applierboy struct {
-	WriteFile func(filename string, content string) error
-}
-
-func writeFile(filename string, content string) error {
-	var dir = path.Dir(filename)
-
-	var error = os.MkdirAll(dir, os.ModePerm)
-
-	if error != nil {
-		return internal.AplyError{
-			Description:   "Problem creating new folder for file",
-			InternalError: error,
-		}
-	}
-
-	return ioutil.WriteFile(filename, []byte(content), os.ModePerm)
-}
-
-func (ab *applierboy) instantiate() {
-	ab.WriteFile = writeFile
+	instantiated bool
+	writeFile    fileWriter
+	createFolder folderCreator
 }
 
 // Install installs the hooks with the given configuration
 func (ab applierboy) Install(configuration conf.Configuration, ftc []internal.FileToCreate) (string, error) {
 	ab.instantiate()
 
-	return writeFiles(ab.WriteFile, ftc)
-}
-
-func writeFiles(writeFile func(filename string, content string) error, filesToCreate []internal.FileToCreate) (string, error) {
-	for _, ftc := range filesToCreate {
+	for _, ftc := range ftc {
 		var content = ftc.Contents
 		var fullFileName = ftc.Path
-		var createHookFileError = writeFile(fullFileName, content)
 
-		if createHookFileError != nil {
-			return "", createHookFileError
+		var dir = path.Dir(fullFileName)
+
+		err := ab.createFolder(dir, os.ModePerm)
+
+		if err != nil {
+			return "", internal.AplyError{
+				Description:   "Problem creating new folder for file",
+				InternalError: err,
+			}
+		}
+
+		err = ab.writeFile(fullFileName, []byte(content), os.ModePerm)
+
+		if err != nil {
+			return "", internal.AplyError{
+				Description:   "Problem creating file",
+				InternalError: err,
+			}
 		}
 	}
 
 	return hooksInstalledMessage, nil
+}
+
+func (ab *applierboy) instantiate() {
+	if !ab.instantiated {
+		ab.writeFile = ioutil.WriteFile
+		ab.createFolder = os.MkdirAll
+		ab.instantiated = true
+	}
 }
 
 var hooksInstalledMessage = "Hooks installed!"
--- source/hookboy/aply/applier_test.go
+++ source/hookboy/aply/applier_test.go
@@ -1,6 +1,7 @@
 package aply
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -48,10 +49,72 @@ func Test_applier_InstallWritesFilesToFolderThatDidNotExistBefore(t *testing.T) 
 	}
 }
 
-type simpleFileForTest struct {
-	name string
+func Test_applier_InstallPropagatesFileError(t *testing.T) {
+	c := conf.Configuration{}
+	ftc := internal.FileToCreate{
+		Contents: "DoesNotMatter",
+		Path:     "DoesNotMatter",
+	}
+
+	errorToReturn := errors.New("File writing failed")
+	errorReturningWrite := func(string, []byte, os.FileMode) error {
+		return errorToReturn
+	}
+	nonErrorReturningFolderCreate := func(path string, perm os.FileMode) error {
+		return nil
+	}
+	var applier = applierboy{
+		writeFile:    errorReturningWrite,
+		createFolder: nonErrorReturningFolderCreate,
+		instantiated: true,
+	}
+
+	_, err := applier.Install(c, []internal.FileToCreate{ftc})
+
+	aplyError, ok := err.(internal.AplyError)
+
+	if !ok {
+		t.Error("Expected error to be of type AplyError")
+		return
+	}
+
+	expectedMessage := "Problem creating file"
+	actualMessage := aplyError.Error()
+	if expectedMessage != actualMessage {
+		t.Error("AplyError does not have the correct error message")
+		return
+	}
 }
 
-func (sf simpleFileForTest) Name() string {
-	return sf.name
+func Test_applier_InstallPropagatesFolderError(t *testing.T) {
+	c := conf.Configuration{}
+	ftc := internal.FileToCreate{
+		Contents: "DoesNotMatter",
+		Path:     "DoesNotMatter",
+	}
+
+	errorToReturn := errors.New("DoesNotMatter")
+	errorReturningFolderCreate := func(path string, perm os.FileMode) error {
+		return errorToReturn
+	}
+	var applier = applierboy{
+		createFolder: errorReturningFolderCreate,
+		instantiated: true,
+	}
+
+	_, err := applier.Install(c, []internal.FileToCreate{ftc})
+
+	aplyError, ok := err.(internal.AplyError)
+
+	if !ok {
+		t.Error("Expected error to be of type AplyError")
+		return
+	}
+
+	expectedMessage := "Problem creating new folder for file"
+	actualMessage := aplyError.Error()
+	if expectedMessage != actualMessage {
+		t.Error("AplyError does not have the correct error message")
+		return
+	}
 }
